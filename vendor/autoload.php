87f655c3921f8b6be0e478be6538737db76b60b4
cafedcup <39214995+cafedcup@users.noreply.github.com>
2018-05-12 18:52:45 +0700

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -2,7 +2,7 @@
 require "vendor/autoload.php";
 $access_token = '4Qu7kgrFlDwTEszsj7jmLBOiQZlJ8VPm0Cl6cgPBD68TguSuDKlCO7fb/hQojMXf9elSUa6VQ6iAm0SiVmUxQlRbnOFN38rCMclfZ/2EfLH1O4mzPPEG8RiF3yv99r2+aRHOS+usOHxGQ882dov5owdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '225c1cb58f767eaf6b61053c1346727f';
-$idPush = 'U0f8ed013f50650deb6a9e0a95042d4b0'
+$idPush = 'U0f8ed013f50650deb6a9e0a95042d4b0';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
